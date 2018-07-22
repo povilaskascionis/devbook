ded289e78dc2d44d9758608c1009e1e4f680d296
unknown <povilas.kascionis@gmail.com>
2018-07-22 20:46:34 +0300

    add education, experience and profile delete routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -252,4 +252,73 @@ Router.post(
   }
 );
 
+// @route DELETE to api/profile/experience/:exp_id
+// @desc delete an experience from the profile
+// @access Private
+
+Router.delete(
+  '/experience/:exp_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //Get index of experience to be removed
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        //Splice experience from array
+        profile.experience.splice(removeIndex, 1);
+        profile
+          .save()
+          .then(profile => res.json(profile))
+          .catch(err => res.json(err));
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  }
+);
+
+// @route DELETE to api/profile/education/:edu_id
+// @desc delete an education from the profile
+// @access Private
+Router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        profile.education.splice(removeIndex, 1);
+        profile
+          .save()
+          .then(profile => res.json(profile))
+          .catch(err => res.json(err));
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  }
+);
+
+// @route DELETE to api/profile
+// @desc delete user and profile
+// @access Private
+
+Router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findByIdAndRemove({ user: req.user.id }).then(() => {
+      User.findByIdAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
+
 module.exports = Router;
