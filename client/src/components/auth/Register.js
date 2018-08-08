2f4aec4bff83ac803660425f4e06a93ab324c870
unknown <povilas.kascionis@gmail.com>
2018-08-08 21:18:21 +0300

    add react-app and set landing, footer and header components

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Register</h4>
+      </div>
+    );
+  }
+}
+
+export default Register;
